b441f75bceacf00ff0fb66ecee81569b43a0ab90
gustavohenri316 <gustavohenri316@gmail.com>
2022-07-22 16:35:34 -0300

    first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,16 @@
+
+//@ts-ignore
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Inicio | ig.news</title>
+      </Head>
+      <div>
+        <h1>ola</h1>
+      </div>
+    </>
+  )
+}
